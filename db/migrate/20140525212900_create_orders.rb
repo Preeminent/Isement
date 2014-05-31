583843868a52e00a5b449cb5a02d5384edb92b23
Anthony <anthony@preeminentproductions.com>
2014-05-31 14:52:08 -0700

    deleted unneeded items in orders database

--- db/migrate/20140525212900_create_orders.rb
+++ db/migrate/20140525212900_create_orders.rb
@@ -1,13 +1,9 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.string :title
       t.string :address
       t.string :city
       t.string :state
-      t.string :description
-      t.integer :rate
-
       t.timestamps
     end
   end
