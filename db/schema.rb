cca6e79fafc8ff264819c39cadffd6258443e55f
Anthony <anthony@preeminentproductions.com>
2014-05-31 15:55:52 -0700

    added bank account transfer function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525214238) do
+ActiveRecord::Schema.define(version: 20140531223054) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140525214238) do
     t.string   "name"
     t.string   "state"
     t.string   "city"
+    t.string   "recipient"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
